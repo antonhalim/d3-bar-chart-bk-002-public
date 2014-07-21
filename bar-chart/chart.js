dd8afcbcc98d264e1468cb041e8c64d279fcadf8
Katie Hoffman <ktahoffman@gmail.com>
2014-07-21 15:44:13 -0400

    first step, requires files in index.html

---
+++ bar-chart/chart.js
@@ -0,0 +1,20 @@
+var election = [4, 8, 15, 16, 23, 42];
+
+////////// v  code goes below v //////////
+
+
+
+
+///////// ^ code goes above ^ //////////
+
+setTimeout(function() {
+  election[2].value = 30;
+}, 500);
+
+setTimeout(function() {
+  election[3].value = 9;
+}, 1000);
+
+setTimeout(function() {
+  election[4].value = 35;
+}, 1500);
