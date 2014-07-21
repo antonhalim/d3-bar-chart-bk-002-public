c8110cdec5bc4c10ffe10037c51449aee0ce362a
Katie Hoffman <ktahoffman@gmail.com>
2014-07-21 16:20:15 -0400

    makes fucntional bar chart

--- bar-chart/chart.js
+++ bar-chart/chart.js
@@ -1,20 +1,41 @@
-var election = [4, 8, 15, 16, 23, 42];
+var election = 
+  [
+    {"region":"Alamosa", "value":  4},
+    {"region":"Douglas", "value":  8},
+    {"region":"Jackson", "value": 15},
+    {"region":"Mesa"   , "value": 16},
+    {"region":"Pueblo" , "value": 23}
+  ];
 
 ////////// v  code goes below v //////////
+var maxValue = 0;
+for (var i=0; i < election.length; i++) {
+  if(election[i].value > maxValue) {
+    maxValue = election[i].value;
+  }
+}
 
+var x = d3.scale.linear()
+    .domain([0, maxValue])
+    .range([0, 420]);
 
-
+d3.select(".bar-chart")
+  .selectAll("div")
+    .data(election)
+  .enter().append("div")
+    .style("width", function(d) { return x(d.value) + "px"; })
+    .text(function(d) { return d.region; });
 
 ///////// ^ code goes above ^ //////////
 
-setTimeout(function() {
-  election[2].value = 30;
-}, 500);
+// setTimeout(function() {
+//   election[2].value = 30;
+// }, 500);
 
-setTimeout(function() {
-  election[3].value = 9;
-}, 1000);
+// setTimeout(function() {
+//   election[3].value = 9;
+// }, 1000);
 
-setTimeout(function() {
-  election[4].value = 35;
-}, 1500);
+// setTimeout(function() {
+//   election[4].value = 35;
+// }, 1500);
