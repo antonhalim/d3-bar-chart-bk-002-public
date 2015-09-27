b209ffe695a16c126484ae7fb660a30991b6fc27
Anton Halim <axton_n@yahoo.com>
2015-09-26 22:26:57 -0400

    done

--- bar-chart/chart.js
+++ bar-chart/chart.js
@@ -1,4 +1,4 @@
-var election = 
+var election =
   [
     {"region":"Alamosa", "value":  4},
     {"region":"Boulder", "value":  6},
@@ -10,5 +10,25 @@ var election =
     {"region":"Pueblo" , "value": 40}
   ];
 
-////////// v  code goes below v //////////
+var x = d3.scale.linear();
+var maxTurnOut = function(elect){
+    for(var i = 0, res = 0; i < election.length; i++){
+        if (election[i]['value'] > res){
+            res = election[i]['value'];
+        }
+    }
+    return res;
+}(election);
+x.range([0, 420]);
+x.domain([0, maxTurnOut]);
 
+var chart = d3.select('.bar-chart');
+var dataBound = chart.selectAll('div').data(election);
+
+var enter = dataBound.enter();
+var enterBar = dataBound.enter().append('div');
+
+enterBar.style("width", function(d) { return x(d.value) + "px"; });
+enterBar.text(function(d){
+    return d.region;
+})
